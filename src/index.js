50dbcb211d006386c777d3d07fa6f4ecce3db2a8
BrunoGuerrero <bruno@guidap.co>
2018-10-02 17:33:35 +0200

    NOUVEAU MODULE - Injection de page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,71 @@
-let rootElement =  document.createElement('div');
+(function() {
+    let GUIDAP = {};
 
-let rootContent =  document.createElement('div');
-    rootContent.setAttribute('style', 'position:absolute; width:96vw; height:96vh; padding:10px; background-color:red; left:2vw; top:2vh; box-sizing: border-box;');
+    if(window.GUIDAP) {
+        GUIDAP = window.GUIDAP;
+    }
 
-let closeElement = document.createElement('div');
-closeElement.setAttribute('style', 'position:absolute; width:50px; height:50px; top:-10px; right:-10px; border-radius:50%; border:1px solid black; background:white;');
-closeElement.setAttribute("width", Math.max(document.documentElement.clientWidth, window.innerWidth || 0).toString());
+    function Booking() {
 
-rootContent.appendChild(closeElement);
+        this.domain = null;
+        this.uuid = null;
 
-let iframeElement = document.createElement('iframe');
-iframeElement.setAttribute('src', 'http://dev1.test/app_dev.php/activity/export-full/ZvpREVce36w97At4B2dUHWShLaxIQTj1Ymkl');
-iframeElement.setAttribute('style', 'width:100%; height:100%');
-iframeElement.setAttribute("width", Math.max(document.documentElement.clientWidth, window.innerWidth || 0).toString());
+        this.rootElement = null;
+        this.contentElement = null;
+        this.iframeElement = null;
 
-rootContent.appendChild(iframeElement);
+        /** Sets iframe src and display modal
+         * @param uuid Uuid of module export
+         */
+        this.call = function (uuid) {
+            this.iframeElement.setAttribute('src', 'https://' + this.domain + '/app_dev.php/activity/export-full/' + uuid);
+            this.rootElement.style.display = 'block';
+        };
 
-rootElement.appendChild(rootContent);
+        /** Closes modal
+         */
+        this.close = function () {
+            this.iframeElement.setAttribute('src', '');
+            this.rootElement.style.display = 'none';
+        };
 
-document.body.appendChild(rootElement);
+        /** Sets owner domain for use in iframe, creates modal
+         * @param domain Tenant domain
+         */
+        this.init = function (domain) {
+            this.domain = domain;
+
+            // Creates a full page veil
+            this.rootElement = document.createElement('div');
+            this.rootElement.setAttribute('style', 'display:none; position:absolute; left:0; top:0; height:100vh; width:100vw; background-color:rgba(50, 50, 50, 0.5);');
+
+            // Creates a modal on veil
+            this.contentElement = document.createElement('div');
+            this.contentElement.setAttribute('style', 'position:absolute; top:10px; left:10px; width:calc(100% - 20px); height:calc(100% - 20px); box-sizing: border-box; border:1px solid #E0E0E0; background-color:#fff; box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);');
+
+            // Creates a close button
+            let closeElement = document.createElement('div');
+            closeElement.setAttribute('style', 'position:absolute; height:50px; width: 50px; top:0; right:0; background:rgba(224, 224, 224, 0.5); line-height:50px; text-align:center; font-family: sans-serif; cursor:pointer;');
+            closeElement.setAttribute('onClick', 'GUIDAP.booking.close()');
+            closeElement.innerHTML = 'X';
+            this.contentElement.appendChild(closeElement);
+
+            // Creates iframe used to display activity page
+            this.iframeElement = document.createElement('iframe');
+            this.iframeElement.setAttribute('style', 'width:100%; height:100%');
+            this.iframeElement.setAttribute('marginwidth', "0");
+            this.iframeElement.setAttribute('marginHeight', "0");
+            this.iframeElement.setAttribute('frameborder', "0");
+            this.iframeElement.setAttribute('vspace', "0");
+            this.iframeElement.setAttribute('hspace', "0");
+            this.iframeElement.setAttribute('allowtransparency', "0");
+            this.contentElement.appendChild(this.iframeElement);
+
+            this.rootElement.appendChild(this.contentElement);
+            document.body.appendChild(this.rootElement);
+        };
+    };
+
+    GUIDAP.booking = new Booking();
+    window.GUIDAP = GUIDAP;
+})();
