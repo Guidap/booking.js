37022e5e0553ecab51d711c8bef3c944ddc0d999
BrunoGuerrero <bruno@guidap.co>
2018-10-03 17:32:20 +0200

    NOUVEAUX EXPORTS - Finalisation design modale

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@
          * @param e JS event from onclick
          */
         this.call = function (uuid, e) {
-
             let event;
             if (!e) {
                 event = window.event;
@@ -54,17 +53,17 @@
 
             // Creates a full page veil
             this.rootElement = document.createElement('div');
-            this.rootElement.setAttribute('style', 'display:none; position:absolute; left:0; top:0; height:100vh; width:100vw; background-color:rgba(50, 50, 50, 0.5);');
+            this.rootElement.setAttribute('style', 'display:none; position:fixed; left:0; top:0; height:100vh; width:100vw; background-color:rgba(50, 50, 50, 0.5);');
 
             // Creates a modal on veil
             this.contentElement = document.createElement('div');
-            this.contentElement.setAttribute('style', 'position:absolute; top:10px; left:10px; width:calc(100% - 20px); height:calc(100% - 20px); box-sizing: border-box; border:1px solid #E0E0E0; background-color:#fff; box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);');
+            this.contentElement.setAttribute('style', 'position:absolute; left:10px; top:25px; width:calc(100% - 20px); height:calc(100% - 50px); box-sizing: border-box; border:1px solid #E0E0E0; background-color:#fff; box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);');
 
             // Creates a close button
             let closeElement = document.createElement('div');
-            closeElement.setAttribute('style', 'position:absolute; height:50px; width: 50px; top:0; right:0; background:rgba(224, 224, 224, 0.5); line-height:50px; text-align:center; font-family: sans-serif; cursor:pointer;');
+            closeElement.setAttribute('style', 'position:absolute; top:-22px; right:-8px; height:44px; width:44px; border-radius:50%; background:#fff; text-align:center; font-family: sans-serif; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.14), 0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px rgba(0, 0, 0, 0.20); cursor:pointer;');
             closeElement.setAttribute('onClick', 'GUIDAP.booking.close()');
-            closeElement.innerHTML = 'X';
+            closeElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="height:100%" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path fill="4c4c4c" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>';
             this.contentElement.appendChild(closeElement);
 
             // Creates iframe used to display activity page
